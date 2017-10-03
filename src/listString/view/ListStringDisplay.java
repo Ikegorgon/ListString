1f21e7b94094438f0dc5f329b54c817a2a10f433
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-03 12:29:58 -0600

    Made display for project

--- src/listString/view/ListStringDisplay.java
+++ src/listString/view/ListStringDisplay.java
@@ -1,5 +1,15 @@
 package listString.view;
 
-public class ListStringDisplay {
+import javax.swing.JOptionPane;
 
+public class ListStringDisplay {
+	public void displayText (String textToDisplay) {
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	public String getResponse(String fromQuestion) {
+		String answer = "";
+		answer += JOptionPane.showInputDialog(null, fromQuestion);
+		return answer;
+	}
+	
 }
