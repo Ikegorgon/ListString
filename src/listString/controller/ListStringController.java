73310c6f5293a3942a0368986dea8b34dcad66b1
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-10-05 13:18:24 -0600

    Added Topic String Variable

--- src/listString/controller/ListStringController.java
+++ src/listString/controller/ListStringController.java
@@ -20,16 +20,28 @@ public class ListStringController {
 		showTheList();
 	}
 	private void showTheList() {
+		String currentCreator = "";
 		for (int i = 0; i < myKahoots.size(); i++) {
+			currentCreator = myKahoots.get(i).getCreator();
+			Kahoot currentKahoot = myKahoots.get(i);
+			String creator = currentKahoot.getCreator();
 			popup.displayText(myKahoots.get(i).toString());
+			if (currentCreator.equals("")) {
+				for (int loop = 0; loop < 5; loop++) {
+					popup.displayText("Who made this crap?");
+				}
+			}
+			for (int CurrentLetterIndex = 0; CurrentLetterIndex < creator.length(); CurrentLetterIndex++) {
+				popup.displayText(currentCreator.substring(CurrentLetterIndex, CurrentLetterIndex + 1));
+			}
 		}
 	}
 	private void fillTheList() {
-		Kahoot fiftyStates = new Kahoot(50, 2, "John", true);
-		Kahoot abc = new Kahoot(26, 1, "Bob", false);
-		Kahoot num = new Kahoot(20, 3, "Patty", true);
-		Kahoot fruit = new Kahoot(12, 5, "George", true);
-		Kahoot pie = new Kahoot(1, 1, "Tanner", false);
+		Kahoot fiftyStates = new Kahoot(50, 2, "John", true, "States");
+		Kahoot abc = new Kahoot(26, 1, "Bob", false, "ABC's");
+		Kahoot num = new Kahoot(20, 3, "Patty", true, "Numbers");
+		Kahoot fruit = new Kahoot(12, 5, "George", true, "Fruits");
+		Kahoot pie = new Kahoot(1, 1, "Tanner", false, "Pie");
 		myKahoots.add(fiftyStates);
 		myKahoots.add(abc);
 		myKahoots.add(num);
--- src/listString/model/Kahoot.java
+++ src/listString/model/Kahoot.java
@@ -5,19 +5,22 @@ public class Kahoot {
 	private int level;
 	private String creator;
 	private boolean isJumble;
+	private String topic;
 	
 	public Kahoot() {
 		questionCount = 0;
 		level = 0;
 		creator = "";
 		isJumble = false;
+		topic = "";
 	}
 	
-	public Kahoot(int questionCount, int level, String creator, boolean isJumble) {
+	public Kahoot(int questionCount, int level, String creator, boolean isJumble, String topic) {
 		this.questionCount = questionCount;
 		this.level = level;
 		this.creator = creator;
 		this.isJumble = isJumble;
+		this.topic = topic;
 	}
 	
 	public String toString() {
@@ -27,7 +30,7 @@ public class Kahoot {
 		} else {
 			Jumble += "is not Jumbled.";
 		}
-		String description = "This Kahoot has " + questionCount + " questions, is level " + 
+		String description = "This Kahoot about " + topic + " has " + questionCount + " questions, is level " + 
 	level + ", was made by " + creator + ", and " + Jumble;
 		
 		return description;
@@ -45,6 +48,9 @@ public class Kahoot {
 	public boolean getIsJumble() {
 		return isJumble;
 	}
+	public String getTopic() {
+		return topic;
+	}
 	
 	public void setQuestionCount(int questionCount) {
 		this.questionCount = questionCount;
@@ -57,5 +63,8 @@ public class Kahoot {
 	}
 	public void setIsJumble(boolean isJumble) {
 		this.isJumble = isJumble;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
 	}
 }
